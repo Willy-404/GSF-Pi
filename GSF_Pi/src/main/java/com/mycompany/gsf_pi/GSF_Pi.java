44f11a37a0f373a32d20c97ea04a5f33ce4ffd54
unknown <felipe.w28@aluno.ifsc.edu.br>
2025-03-10 10:11:29 -0300

    Projeto inicial

---
+++ GSF_Pi/src/main/java/com/mycompany/gsf_pi/GSF_Pi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gsf_pi;
+
+/**
+ *
+ * @author Aluno
+ */
+public class GSF_Pi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
