d18a8d4ab7096b7b83c1d9cf50f3f8df13ed85b1
Gustavo <gusgermes@gmail.com>
2025-03-11 12:06:05 -0300

    Tela de login
    Tela de login, falta programar

---
+++ GsfPI/src/main/java/Controller/LoginController.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button Btnlogin;
+
+    @FXML
+    private Button btnircadastro;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField senha;
+
+    @FXML
+    void onclicklogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onclicktelacadastro(ActionEvent event) {
+
+    }
+
+}
