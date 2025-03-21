127462cfe032f2024906631f07e2c9498a44b381
Pedro <ph1953785@gmail.com>
2025-03-21 15:33:22 -0300

    Fluxo do Menu Rapido
    Aplicado em Todas as telas

--- GsfPI/src/main/java/Controller/VisualizarPontoController.java
+++ GsfPI/src/main/java/Controller/VisualizarPontoController.java
@@ -1,11 +1,16 @@
 package Controller;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 
 public class VisualizarPontoController {
 
@@ -46,43 +51,107 @@ public class VisualizarPontoController {
     private Menu menuVisualizar;
 
     @FXML
-    void OnClickCadFornecedor1(ActionEvent event) {
-
+    void OnClickCadFornecedor1(ActionEvent event) throws IOException {
+        Stage cadastroFornecedor = new Stage();
+        cadastroFornecedor.setTitle("Cadastro de Fornecedor");
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CadastrarFornecedor.fxml"));
+        Parent root= loader.load();
+        
+       Scene cena = new Scene(root);
+       cadastroFornecedor.setScene(cena);
+       cadastroFornecedor.show(); 
     }
 
     @FXML
-    void OnClickCadFuncionario1(ActionEvent event) {
-
+    void OnClickCadFuncionario1(ActionEvent event) throws IOException {
+         Stage cadastroFuncionario = new Stage();
+        cadastroFuncionario.setTitle("Cadastro de Funcionario");
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CadastrarFuncionario.fxml"));
+        Parent root= loader.load();
+        
+       Scene cena = new Scene(root);
+       cadastroFuncionario.setScene(cena);
+       cadastroFuncionario.show(); 
     }
 
     @FXML
-    void OnClickCadLote1(ActionEvent event) {
-
+    void OnClickCadLote1(ActionEvent event) throws IOException {
+         Stage cadastroLote = new Stage();
+        cadastroLote.setTitle("Cadastro de lotes");
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CadastroLotes.fxml"));
+        Parent root= loader.load();
+        
+       Scene cena = new Scene(root);
+       cadastroLote.setScene(cena);
+       cadastroLote.show(); 
     }
 
     @FXML
-    void OnClickVisuFornecedor1(ActionEvent event) {
-
+    void OnClickVisuFornecedor1(ActionEvent event) throws IOException {
+ Stage visuFornecedor = new Stage();
+        visuFornecedor.setTitle("VisualizarFornecedor");
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/VisualizarFornecedor.fxml"));
+        Parent root= loader.load();
+        
+       Scene cena = new Scene(root);
+       visuFornecedor.setScene(cena);
+       visuFornecedor.show(); 
     }
 
     @FXML
-    void OnClickVisuFuncionario1(ActionEvent event) {
-
+    void OnClickVisuFuncionario1(ActionEvent event) throws IOException {
+Stage visuFuncionario = new Stage();
+        visuFuncionario.setTitle("VisualizarFuncionario");
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/VisualizarFuncionario.fxml"));
+        Parent root= loader.load();
+        
+       Scene cena = new Scene(root);
+       visuFuncionario.setScene(cena);
+       visuFuncionario.show();
     }
 
     @FXML
-    void OnClickVisuLote1(ActionEvent event) {
-
+    void OnClickVisuLote1(ActionEvent event) throws IOException {
+  Stage visuLotes = new Stage();
+        visuLotes.setTitle("Visualizar Lotes");
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/VisualizarLotes.fxml"));
+        Parent root= loader.load();
+        
+       Scene cena = new Scene(root);
+       visuLotes.setScene(cena);
+       visuLotes.show(); 
     }
 
     @FXML
-    void OnClickVisuPonto1(ActionEvent event) {
-
+    void OnClickVisuPonto1(ActionEvent event) throws IOException {
+       Stage visuPonto = new Stage();
+        visuPonto.setTitle("Visualizar Pontos");
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/VisualizarPonto.fxml"));
+        Parent root= loader.load();
+        
+       Scene cena = new Scene(root);
+       visuPonto.setScene(cena);
+       visuPonto.show(); 
     }
 
     @FXML
-    void OnClickVisuTelaHome(ActionEvent event) {
-
+    void OnClickVisuTelaHome(ActionEvent event) throws IOException {
+  Stage home = new Stage();
+        home.setTitle("home");
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/TelaHome.fxml"));
+        Parent root= loader.load();
+        
+       Scene cena = new Scene(root);
+       home.setScene(cena);
+       home.show(); 
     }
 
 }
