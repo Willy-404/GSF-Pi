59b01e145825d36bd6ac9381032f5c3369bef074
Larissa <Larissa.vsl@aluno.ifsc.edu.br>
2025-03-25 11:17:57 -0300

    Iniciando o banco de dados

---
+++ GsfPI/src/main/java/dal/ConexaoBD.java
@@ -0,0 +1,36 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBD {
+    private static final String URL_MYSQL = "jdbc:mysql://localhost:3306/gsf";
+    private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
+    private static final String USER = "root";
+    private static final String PASSWORD = "aluno";
+    
+ private static Connection conectar(){
+    try {
+            Class.forName(DRIVER_CLASS);
+            return DriverManager.getConnection(
+                    URL_MYSQL, USER, PASSWORD);
+        } catch(ClassNotFoundException | SQLException e){
+            e.getMessage();
+        }
+        return null;
+ }
+  public void desconectar(Connection conexao){
+        try{
+            conexao.close();
+        } catch(SQLException e){
+            e.getMessage();
+        }
+    }
+    
+    
+}
+
+ 
+    
+
--- GsfPI/src/main/java/module-info.java
+++ GsfPI/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module principal {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens Controller to javafx.fxml;
     exports principal;
