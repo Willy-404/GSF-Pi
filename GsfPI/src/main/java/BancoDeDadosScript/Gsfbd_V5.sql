58565e7c6bb3ebfebf511e9ed2212cb5c9ab4be4
Felipe <felipe.w28@aluno.ifsc.edu.br>
2025-05-27 09:44:22 -0300

    banco com create schema

--- GsfPI/src/main/java/BancoDeDadosScript/Gsfbd_V5.sql
+++ GsfPI/src/main/java/BancoDeDadosScript/Gsfbd_V5.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `gsf` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `gsf`;
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
 -- Host: localhost    Database: gsf
@@ -310,4 +312,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-26 10:58:26
+-- Dump completed on 2025-05-27  9:43:53
